9d04400c8ca1700bb7a9825ec6cd55ff1fdda15d
[kaneka vong] <[kanekavong@gmail.com]>
2025-07-21 23:35:29 +0700

    Prepare for Render deployment

--- ecommersWebsite/settings.py
+++ ecommersWebsite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ygf3^9q(pwz$ei$nkls$s88pxz^-j=m5mk9*6l)u_9^(zcy7&0'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "unsafe-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
