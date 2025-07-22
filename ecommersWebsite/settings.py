fb61e807f6248fb185118b5c1764fcdbf5470353
[kaneka vong] <[kanekavong@gmail.com]>
2025-07-22 12:59:51 +0700

    change allowed host

--- ecommersWebsite/settings.py
+++ ecommersWebsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "unsafe-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['Kaneka235.pythonanywhere.com']
 
 
 # Application definition
